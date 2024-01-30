163d937e794f0856e314ba1b5b7df609404d154d
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-01-29 23:23:20 -0600

    Create header.js

---
+++ js/header.js
@@ -0,0 +1,18 @@
+document.write(`
+<header>
+  <nav>
+    <a href="/">Home</a>
+    <a href="/g/">Games</a>
+    <a href="/p/">Proxy</a>
+    <a href="/e/b.html">Bookmarklets</a>
+    <a href="/e/m.html">Mirrors</a>
+    <a href="/s/"><i class="fas fa-search"></i></a>
+    <a href="/e/s.html">Settings</a>
+  </nav>
+  <div id="socials">
+  <a href="https://github.com/CybriaTech/Opium-1.0" title="Github"><i class="fab fa-github"></i></a>
+  <a href="https://bit.ly/cybriatech" title="Discord"><i class="fab fa-discord"></i></a>
+  <a href="/e/w.html" title="Widgetbot"><i class="fas fa-comment"></i></a>
+  </div>
+</header>
+`);
