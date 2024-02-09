501ff99cdcf52120a270d2028ff57997d688854f
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-02-08 21:10:37 -0600

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -3,7 +3,7 @@ document.write(`
       <p>&copy; ${new Date().getFullYear()} CybriaTech</p>
       <p style="margin-left: 5px; font-size: 1.7em;">&#x2022;</p>
       <a href="/e/changelog.html">Changelog</a>
-      <a href="/e/about.html">About</a>
       <a href="/e/donate.html">Donate</a> 
+      <a href="/e/api.html">API</a>
   </footer>
 `);
