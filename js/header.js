27bcc95a4ee5fb9c84a94a88e80d6e96d7bfbb44
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-02-15 15:45:38 +0000

    Update header.js

--- js/header.js
+++ js/header.js
@@ -13,7 +13,7 @@ document.write(`
       <input type="text" name="g" class="search-bar-top" placeholder="Search For A Game">
       <button class="search-enter-top"><i class="fas fa-search"></i></button>
       </form>
-      <a href="/e/s.html"><i class="fas fa-cog"></i></a>
+      <a style="justify-content: flex-end;" href="/e/s.html"><i class="fas fa-cog"></i></a>
   </div>
 </header>
 `);
