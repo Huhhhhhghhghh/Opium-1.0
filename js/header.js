17c1d27740d1ec82cbe76fcf1b40d661be79f4b0
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-02-15 15:34:42 +0000

    Update header.js

--- js/header.js
+++ js/header.js
@@ -8,8 +8,12 @@ document.write(`
     <a href="/e/m.html">Mirrors</a>
     <a href="/e/c.html">Changelog</a>
   </nav>
-  <div id="socials">
-
+  <div id="right">
+        <form action="/search" method="get">
+      <input type="text" name="g" class="search-bar" placeholder="Search For A Game">
+      <button class="search-enter"><i class="fas fa-search"></i></button>
+      </form>
+      <a href="/e/s.html"><i class="fas fa-cog"></i></a>
   </div>
 </header>
 `);
