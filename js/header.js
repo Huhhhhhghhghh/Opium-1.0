ad2e93ff4eab8503eb82a69601590fe30cf9613d
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-02-15 15:32:40 +0000

    Update header.js

--- js/header.js
+++ js/header.js
@@ -6,14 +6,10 @@ document.write(`
     <a href="/p/">Proxy</a>
     <a href="/e/b.html">Bookmarklets</a>
     <a href="/e/m.html">Mirrors</a>
-    <a href="/e/s.html">Settings</a>
+    <a href="/e/c.html">Changelog</a>
   </nav>
   <div id="socials">
-  <a href="https://github.com/CybriaTech/Opium-1.0" title="Github"><i class="fab fa-github"></i></a>
-  <a href="https://bit.ly/cybriatech" title="Discord"><i class="fab fa-discord"></i></a>
-  <a href="https://twitter.com/cybriatech_" title="Twitter (X)"><i class="fab fa-twitter"></i></a>
-  <a href="https://tiktok.com/@cybriatech" title="TikTok"><i class="fab fa-tiktok"></i></a>
-  <a href="/e/w.html" title="Widgetbot"><i class="fas fa-comment"></i></a>
+
   </div>
 </header>
 `);
