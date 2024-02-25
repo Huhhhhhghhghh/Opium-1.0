0cb6e5f2fe610f624e03d8f26c28f3a7ecae27c7
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-02-25 06:19:48 +0000

    Update g.js

--- js/g.js
+++ js/g.js
@@ -6,25 +6,27 @@ function ver() {
     fetch('/json/g.json')
     .then(response => response.json())
     .then(data => {
-        data.forEach(game => {
-            game.tags.forEach(tag => {
-                const categoryDiv = document.querySelector(`#${tag}`);
-                if (categoryDiv) {
-                    const gameButton = document.createElement('div');
-                    gameButton.id = 'game-btn';
-                    gameButton.innerHTML = `
-                        <a href="${game.directory}" class="game-button">
-                            <img src="${game.img}" alt="${game.title}">
-                            <div class="underline"></div>
-                        </a>
-                    `;
-                    categoryDiv.appendChild(gameButton);
-                    const gameContainer = document.getElementById('game-container');
-                    if (gameContainer) {
-                        gameContainer.appendChild(gameButton.cloneNode(true));
+       data.forEach(game => {
+            if (game.tags && game.tags.length > 0) {
+                game.tags.forEach(tag => {
+                    const categoryDiv = document.querySelector(`#${tag}`);
+                    if (categoryDiv) {
+                        const gameButton = document.createElement('div');
+                        gameButton.id = 'game-btn';
+                        gameButton.innerHTML = `
+                            <a href="${game.directory}" class="game-button">
+                                <img src="${game.img}" alt="${game.title}">
+                                <div class="underline"></div>
+                            </a>
+                        `;
+                        categoryDiv.appendChild(gameButton);
+                        const gameContainer = document.getElementById('game-container');
+                        if (gameContainer) {
+                            gameContainer.appendChild(gameButton.cloneNode(true));
+                        }
                     }
-                }
-            });
+                });
+            }
         });
     })
     .catch(error => {
