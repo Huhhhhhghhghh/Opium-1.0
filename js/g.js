13c406cddba300957e7d421cd43446d3d5b05a7a
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-02-25 10:44:24 -0600

    Update g.js

--- js/g.js
+++ js/g.js
@@ -11,18 +11,20 @@ function ver() {
                 game.tags.forEach(tag => {
                     const categoryDiv = document.querySelector(`#${tag}`);
                     if (categoryDiv) {
-                        const gameButton = document.createElement('div');
-                        gameButton.id = 'game-btn';
-                        gameButton.innerHTML = `
-                            <a href="${game.directory}" class="game-button">
-                                <img src="${game.img}" alt="${game.title}">
-                                <div class="underline"></div>
-                            </a>
-                        `;
-                        categoryDiv.appendChild(gameButton);
-                        const gameContainer = document.getElementById('game-container');
-                        if (gameContainer) {
-                            gameContainer.appendChild(gameButton.cloneNode(true));
+                        if (game.tags.includes(tag)) {
+                            const gameButton = document.createElement('div');
+                            gameButton.id = 'game-btn';
+                            gameButton.innerHTML = `
+                                <a href="${game.directory}" class="game-button">
+                                    <img src="${game.img}" alt="${game.title}">
+                                    <div class="underline"></div>
+                                </a>
+                            `;
+                            categoryDiv.appendChild(gameButton);
+                            const gameContainer = document.getElementById('game-container');
+                            if (gameContainer) {
+                                gameContainer.appendChild(gameButton.cloneNode(true));
+                            }
                         }
                     }
                 });
