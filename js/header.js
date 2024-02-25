ef7f9a19af5941034067893a866c332a4dc7c728
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-02-24 20:00:38 -0600

    theme toggle tst

--- js/header.js
+++ js/header.js
@@ -17,3 +17,38 @@ document.write(`
   </div>
 </header>
 `);
+
+function toggleTheme() {
+  const body = document.body;
+  const themeIcon = document.getElementById('theme-icon');
+  
+  if (body.classList.contains('dark-theme')) {
+    body.classList.remove('dark-theme');
+    themeIcon.classList.remove('fa-moon');
+    themeIcon.classList.add('fa-sun');
+    body.style.backgroundColor = '#e4e3db';
+    body.style.color = '#000';
+    document.querySelectorAll('.mirror-item').forEach(item => {
+      item.style.backgroundColor = '#1C1C1C';
+      item.style.color = '#fff';
+    });
+    document.querySelectorAll('.search-bar, .search-bar-top').forEach(bar => {
+      bar.style.backgroundColor = '#35363E';
+      bar.style.color = '#fff';
+    });
+  } else {
+    body.classList.add('dark-theme');
+    themeIcon.classList.remove('fa-sun');
+    themeIcon.classList.add('fa-moon');
+    body.style.backgroundColor = '#1C1C1C';
+    body.style.color = '#fff';
+    document.querySelectorAll('.mirror-item').forEach(item => {
+      item.style.backgroundColor = '#e0dfd6';
+      item.style.color = '#000';
+    });
+    document.querySelectorAll('.search-bar, .search-bar-top').forEach(bar => {
+      bar.style.backgroundColor = '#e0dfd6';
+      bar.style.color = '#000';
+    });
+  }
+}
