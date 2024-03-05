50c422416303c388e864df8c674e64f51cfda188
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-03-05 03:02:27 +0000

    :trollface:

--- js/g.js
+++ js/g.js
@@ -3,28 +3,36 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function ver() {
+    const addgms = new Map();
+
     fetch('/json/g.json')
     .then(response => response.json())
     .then(data => {
         const gameContainer = document.getElementById('game-container');
-        const addedGames = new Set();
+        
         data.forEach(game => {
             if (game.tags && game.tags.length > 0) {
                 game.tags.forEach(tag => {
                     const categoryDiv = document.querySelector(`#${tag}`);
-                    if (categoryDiv && !addedGames.has(game.title)) {
-                        const gameButton = document.createElement('div');
-                        gameButton.classList.add('game-btn');
-                        gameButton.innerHTML = `
-                            <a href="${game.directory}" class="game-button">
-                             <p class="game-title">${game.title}</p>
-                                <img class="game-img" src="${game.img}" alt="${game.title}">
-                                <div class="underline"></div>
-                            </a>
-                        `;
-                        gameContainer.appendChild(gameButton);
-                        categoryDiv.appendChild(gameButton);
-                        addedGames.add(game.title);
+                    if (categoryDiv) {
+                        if (!addgms.has(tag) || !addgms.get(tag).has(game.title)) {
+                            const gameButton = document.createElement('div');
+                            gameButton.classList.add('game-btn');
+                            gameButton.innerHTML = `
+                                <a href="${game.directory}" class="game-button">
+                                    <p class="game-title">${game.title}</p>
+                                    <img class="game-img" src="${game.img}" alt="${game.title}">
+                                    <div class="underline"></div>
+                                </a>
+                            `;
+                            gameContainer.appendChild(gameButton);
+                            categoryDiv.appendChild(gameButton);
+                            
+                            if (!addgms.has(tag)) {
+                                addgms.set(tag, new Set());
+                            }
+                            addgms.get(tag).add(game.title);
+                        }
                     }
                 });
             }
