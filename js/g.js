74a771c6fc7d60094bc7efc868e50dc2a4c5180c
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-03-22 21:55:21 -0500

    Update g.js

--- js/g.js
+++ js/g.js
@@ -22,6 +22,7 @@ function ver() {
                                 <a href="${game.directory}" class="game-button">
                                     <p class="game-title">${game.title}</p>
                                     <img class="game-img" src="${game.img}" alt="${game.title}">
+                                    <div class="underline"></div>
                                 </a>
                             `;
                             gameContainer.appendChild(gameButton);
